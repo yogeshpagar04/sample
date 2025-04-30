1212f4d55d0520b260f20b3b3687dfedb624f7bb
yogeshpagar04 <yogeshpagar1661@gmail.com>
2025-04-30 19:33:32 +0530

    sample code written

---
+++ script.js
@@ -0,0 +1,11 @@
+let btn1=document.querySelector('#btn');
+btn1.onclick=()=>{
+    console.log("btn1 was clicked")
+    let a=20;
+    a++;
+    console.log(a);
+};
+let box=document.querySelector("div");
+div.onmouseover=()=>{
+    console.log("you are inside div");
+};
